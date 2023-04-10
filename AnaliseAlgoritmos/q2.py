31429f5771bf41b54257e2e1ad66006e186a7a67
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-04-10 20:02:04 -0300

    Create q2.py

---
+++ AnaliseAlgoritmos/q2.py
@@ -0,0 +1,8 @@
+teste = 0
+for i in range(n):
+   teste = teste + 1
+
+for j in range(n):
+   teste = teste - 1
+
+✔️ Apesar de haver dois laços, eles não são aninhados. Você pode entender este caso como O(2n), mas podemos ignorar a constante 2.
