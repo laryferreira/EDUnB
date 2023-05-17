9249ecad4c296fa878679e182532408df148cc5c
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-17 13:11:36 -0300

    Create Drummondichlorian.py

---
+++ pilha&fila/Drummondichlorian.py
@@ -0,0 +1,15 @@
+''''Como parte de seu treinamento, você deve identificar o comportamento dessa nova ferramenta! Dessa forma, dada a sequência de instruções de inserção/remoção em uma drummondichlorian, indique com qual ED tradicional ela se parece.
+
+Entrada
+A entrada contém 5 casos de teste. Para cada caso, a primeira linha apresenta um inteiro 𝑁
+ (0≤𝑁≤100
+) indicando a quantidade de instruções realizadas. As 𝑁
+ linhas seguintes apresentam, cada uma, a instrução para a drummondichlorian no seguinte formato: I X, onde I indica se houve a inserção ("in") de um valor X ou houve a retirada ("out") de um valor (que é X). Considere que 0≤𝑋≤1000
+.
+
+Saída
+Para cada caso de teste, analise o comportamento da drummondichlorian e diga com qual estrutura de dados se parece. 
+As opções são: "LIFO", se ela se comportar como uma pilha, "FIFO", caso tenha o comportamento de uma fila, ou "AIPO" (de any in, priority out), 
+caso pareça ser uma fila de prioridades. Caso o comportamento não seja equivalente a uma destas estruturas de dados tradicionais, informe ao Mestre D'Costa que "no hay!".
+Se houver mais de uma possibilidade, questione-o com: "uai?"'''''
+
