a2d3ebc6138eee1e4080fe1b4ca22b55c111ae11
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-17 19:36:10 -0300

    Create PrevençãodeErros.py

---
+++ Deque&Lista/PrevençãodeErros.py
@@ -0,0 +1,17 @@
+def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+  		previous = current
+                current = current.getNext()
+
+        if found:
+            if previous is None:
+                self.head = current.getNext()
+            else:
+                previous.setNext(current.getNext())
