9cfd93afb014efdbfc1c8d1715ac8d1d949fffcc
Lurian <102330077+luricl@users.noreply.github.com>
2023-05-17 21:13:37 -0300

    Update RemocaoDeDuplicatas.py

--- pilha&fila/RemocaoDeDuplicatas.py
+++ pilha&fila/RemocaoDeDuplicatas.py
@@ -1,3 +1,15 @@
+''' Sua tarefa é, dada uma lista de expressões algébricas válidas, verificar se há algum símbolo de prioridade duplicado dentro de cada expressão.
+Entrada
+A entrada é composta por várias linhas. A primeira linha possui um inteiro N
+(1≤N≤100) que indica o número de expressões a serem verificadas. A seguir, as próximas N linhas possuem, cada uma, um string E
+
+(de comprimento entre 4 e 1000, inclusive), sem espaços, que representa a expressão algébrica a ser analisada.
+
+Saída
+Para cada expressão, a saída deve ser "A expressão possui duplicata.", se houver um símbolo de prioridade duplicado
+dentro da expressão ou "A expressão não possui duplicata." caso não haja um mesmo símbolo de prioridade duplicado 
+dentro da expressão, de acordo com os exemplos.'''
+
 # pilha
 class Stack:
     def __init__(self):
